8658ca3a46737a6c33924f7e865b9e325b26e916
류대현 <eogus1027@naver.com>
2024-12-22 23:09:01 +0900

    id로 학생 정보를 불러오는 API 추가

--- src/main/java/com/example/rest/StudentController.java
+++ src/main/java/com/example/rest/StudentController.java
@@ -2,6 +2,7 @@ package com.example.rest;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudents() {
         return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable Long id) {
+        return studentService.getStudentById(id);
     }
 }
--- src/main/java/com/example/rest/StudentService.java
+++ src/main/java/com/example/rest/StudentService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +26,9 @@ public class StudentService {
 
     public List<Student> getAllStudents() {
         return studentRepository.findAllByOrderByIdDesc();
+    }
+
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).orElseThrow();
     }
 }
