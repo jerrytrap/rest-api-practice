e4f489d04bd67f3cadc87967549550ddd06e627e
류대현 <eogus1027@naver.com>
2024-12-23 17:34:19 +0900

    여러 필드에 에러가 있는 경우 처리

--- src/main/java/com/example/rest/GlobalExceptionHandler.java
+++ src/main/java/com/example/rest/GlobalExceptionHandler.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Comparator;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 @RequiredArgsConstructor
@@ -25,16 +27,20 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<RsData<Void>> handle(MethodArgumentNotValidException e) {
-        FieldError fieldError = e.getBindingResult().getFieldError();
-        String field = fieldError.getField();
-        String code = fieldError.getCode();
-        String message = fieldError.getDefaultMessage();
+        String message = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .filter(error -> error instanceof FieldError)
+                .map(error -> (FieldError) error)
+                .map(error -> error.getField() + "-" + error.getCode() + "-" + error.getDefaultMessage())
+                .sorted(Comparator.comparing(String::toString))
+                .collect(Collectors.joining("\n"));
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new RsData<>(
-                        "400-" + field + "-" + code,
-                        field + ":" + message
+                        "400-1",
+                        message
                 ));
     }
 }
